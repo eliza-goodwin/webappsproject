38d1137952f534ad96a981b70fd5875b8f3ac1e6
DonGitSchool <d00417722@utahtech.edu>
2024-11-07 16:13:14 -0700

    hmm1.0

--- server/server.py
+++ server/server.py
@@ -4,7 +4,7 @@ from plans import PlansDB
 app = Flask(__name__) 
 
 #route for OPTIONS
-@app.route("/plans/<int:plan_id>", methods=["OPTIONS"])
+@app.route("/backend/plans/<int:plan_id>", methods=["OPTIONS"])
 def handle_cors_options(plan_id):
     return "", 204, { #204 = No Content, successful request
         "Access-Control-Allow-Origin": "*",
@@ -13,14 +13,14 @@ def handle_cors_options(plan_id):
     }
     
 #route for GET
-@app.route("/plans", methods=["GET"])
+@app.route("/backend/plans", methods=["GET"])
 def retrieve_plans():
     db = PlansDB("plans_db.db")
     plans = db.getAllPlans()
     return plans, 200, {"Access-Control-Allow-Origin" : "*"}
 
 #route for POST
-@app.route("/plans", methods=["POST"])
+@app.route("/backend/plans", methods=["POST"])
 def create_plan():
     db = PlansDB("plans_db.db")
     print("The request data is: ", request.form)
@@ -34,7 +34,7 @@ def create_plan():
 #QUESTION: does this automatically save in the database? in message log i had to manually save it, is that because it wasn't a database?
     
 #route for PUT
-@app.route("/plans/<int:plans_id>", methods=["PUT"])
+@app.route("/backend/plans/<int:plans_id>", methods=["PUT"])
 def update_plan(plans_id):
     db = PlansDB("plans_db.db")
     print("update plan with ID", plans_id)
@@ -52,7 +52,7 @@ def update_plan(plans_id):
     else:
          return f"Plan with id {plans_id} not found", 404, {"Access-Control-Allow-Origin" : "*"}
 
-@app.route("/plans/<int:plans_id>", methods=["DELETE"])
+@app.route("/backend/plans/<int:plans_id>", methods=["DELETE"])
 def delete_plan(plans_id):
     db = PlansDB("plans_db.db")
     plan = db.getSinglePlan(plans_id)
@@ -62,7 +62,7 @@ def delete_plan(plans_id):
     else:
         return "Not found!!", 404, {"Access-Control-Allow-Origin" : "*"}
 
-@app.route("/plans/<int:plans_id>", methods=["GET"])
+@app.route("/backend/plans/<int:plans_id>", methods=["GET"])
 def retrieve_plan(plans_id):
     db = PlansDB("plans_db.db")
     plan = db.getSinglePlan(plans_id)
@@ -72,7 +72,7 @@ def retrieve_plan(plans_id):
         return "Not found!!", 404, {"Access-Control-Allow-Origin" : "*"}
 
 def run():
-    app.run(port=8080, host='0.0.0.0') #added host='0.0.0.0' for deployment
+    app.run(port=8080, host='0.0.0.0') # Ensure the server runs on port 8080
 
 if __name__ == "__main__":
     run()
